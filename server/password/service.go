06e1d7e7d2e766b8533c51a43940cb59669b1044
ammarteuku05 <ammarteuku09@gmail.com>
2021-06-21 15:18:07 +0700

    update for the last

--- server/password/service.go
+++ server/password/service.go
@@ -1,5 +1,9 @@
 package password
 
+import (
+	"assess/auth"
+)
+
 // type Service interface {
 // 	ShowAllPassowrd(userID string) ([]entity.Password, error)
 // 	CreateNewPassowrd(user entity.PasswordInput) (entity.Password, error)
@@ -7,20 +11,20 @@ package password
 // 	UpdatePassowrdByID(userID string, dataInput entity.PasswordInput) (entity.Password, error)
 // }
 
-// type service struct {
-// 	repository Repository
-// 	auth       auth.Service
-// }
+type service struct {
+	repository Repository
+	auth       auth.Service
+}
 
-// func NewService(repo Repository, auth auth.Service) *service {
-// 	return &service{repo, auth}
-// }
+func NewService(repo Repository, auth auth.Service) *service {
+	return &service{repo, auth}
+}
 
 // func (s *service) ShowAllPassowrd(userID string) ([]entity.Password, error) {
 // 	users, err := s.repository.FindByID(userID)
 
 // 	if err != nil {
-// 		return formatUsers, err
+// 		return users, err
 // 	}
 
 // 	return formatUsers, nil
